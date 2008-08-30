e02b73e485e595515cbace3c71bdc69a3df11581
Don Baccus <dhogaza@pacifier.com>
2008-08-30 01:11:20 +0000

    Let's just say that the list template "display template" with its embedded HTML and its references in a different scope than the caller in the adp file are really annoying and hard to get right the first time.

--- www/admin/index.tcl
+++ www/admin/index.tcl
@@ -17,19 +17,12 @@ set context [list Administer]
 set page_title "Manage Content"
 set name "Content Name"
 
-db_multirow -extend {author_url display_template} revisions revisions {} {
+db_multirow -extend {author_url item_idx element_idx live_revisionx} revisions revisions {} {
     set author_url [export_vars -base /shared/community-member {user_id}]
     set publish_date [lc_time_fmt $publish_date "%x %X"]
-    set display_template "
-        <a href=\"view?element_id=$element_id&item_id=$item_id&revision_id=@revisions.revision_id@\" title=\"#content-includelet.View#\" class=\"button\">#content-includelet.View#</a>
-        <a href=\"edit?element_id=$element_id&item_id=$item_id&revision_id=@revisions.revision_id@\" title=\"#content-includelet.Edit#\" class=\"button\">#content-includelet.Edit#</a>
-        <if @revisions.revision_id@ ne $live_revision>
-          <a href=\"delete?element_id=$element_id&item_id=$item_id&revision_id=@revisions.revision_id@\" title=\"#content-includelet.Delete#\" class=\"button\">#content-includelet.Delete#</a>
-          <a href=\"publish?element_id=$element_id&item_id=$item_id&revision_id=@revisions.revision_id@\" title=\"#content-includelet.Publish#\" class=\"button\">#content-includelet.Publish#</a>
-        </if>
-        <else>
-          <a href=\"unpublish?element_id=$element_id&item_id=$item_id&revision_id=@revisions.revision_id@\" title=\"#content-includelet.Unpublish#\" class=\"button\">#content-includelet.Unpublish#</a>
-        </else>"
+    set item_idx $item_id
+    set element_idx $element_id
+    set live_revisionx $live_revision
 }
 
 template::list::create \
@@ -48,7 +41,17 @@ template::list::create \
         }
         actions {
             label {[_ layout-subsite-integration.Action]}
-            display_template $display_template
+            display_template {
+                <a href="view?element_id=@revisions.element_idx@&item_id=@revisions.item_idx@&revision_id=@revisions.revision_id@" title="#content-includelet.View#" class="button">#content-includelet.View#</a>
+                <a href="edit?element_id=@revisions.element_idx@&item_id=@revisions.item_idx@&revision_id=@revisions.revision_id@" title="#content-includelet.Edit#" class="button">#content-includelet.Edit#</a>
+                <if @revisions.revision_id@ ne @revisions.live_revisionx@>
+                  <a href="delete?element_id=@revisions.element_idx@&item_id=@revisions.item_idx@&revision_id=@revisions.revision_id@" title="#content-includelet.Delete#" class="button">#content-includelet.Delete#</a>
+                  <a href="publish?element_id=@revisions.element_idx@&item_id=@revisions.item_idx@&revision_id=@revisions.revision_id@" title="#content-includelet.Publish#" class="button">#content-includelet.Publish#</a>
+                </if>
+                <else>
+                <a href="unpublish?element_id=@revisions.element_idx@&item_id=@revisions.item_idx@&revision_id=@revisions.revision_id@" title="#content-includelet.Unpublish#" class="button">#content-includelet.Unpublish#</a>
+                </else>
+            }
         }
     }
 
